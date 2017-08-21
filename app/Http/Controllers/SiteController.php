d359665aee06a35e2c74a746be64b06f9b0cbf5c
Artem Garnyshev <is.perfect.possible@gmail.com>
2017-08-21 16:43:59 +0700

    Empty lectures list bugfix

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -244,7 +244,26 @@ class SiteController extends Controller
             unset($fields['subject']['en']);
         }
 
-        if ($fields['subject'] && count($fields['subject']) && (! (isset($fields['subject'][0]) && is_null($fields['subject'][0]))))
+        $emptyList = false;
+        foreach ($fields['subject'] as $k => $value)
+        {
+            if (
+                is_null($fields['subject'][$k]) && 
+                is_null($fields['section'][$k]) && 
+                is_null($fields['theses'][$k])
+            )
+            {
+                unset($fields['subject'][$k]);
+                unset($fields['section'][$k]);
+                unset($fields['theses'][$k]);
+            }
+            else
+            {
+                $emptyList = false;
+            }
+        }
+
+        if ($fields['subject'] && count($fields['subject']) && (!$emptyList))
         {
             $lectures = [];
             $schema = app(\App\Services\SchemaManager::Class)->find('Lectures');
