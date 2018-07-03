b3ddb176d6e708b914e39a32b7cc7c170461950a
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-07-03 16:12:28 +0700

    Translitiration & lecture placeholders

--- resources/assets/js/additional.js
+++ resources/assets/js/additional.js
@@ -2,7 +2,7 @@ function toLatin(text){
 
     var ru = ['Я','я','Ю','ю','Ч','ч','Ш','ш','Щ','щ','Ж','ж','А','а','Б','б','В','в','Г','г','Д','д','Е','е','Ё','ё','З','з','И','и','Й','й','К','к','Л','л','М','м','Н','н', 'О','о','П','п','Р','р','С','с','Т','т','У','у','Ф','ф','Х','х','Ц','ц','Ы','ы','Ь','ь','Ъ','ъ','Э','э'];
 
-    var en = ['Ya','ya','Yu','yu','Ch','ch','Sh','sh','Sh','sh','Zh','zh','A','a','B','b','V','v','G','g','D','d','E','e','E','e','Z','z','I','i','J','j','K','k','L','l','M','m','N','n', 'O','o','P','p','R','r','S','s','T','t','U','u','F','f','H','h','C','c','Y','y','`','`','\'','\'','E', 'e'];
+    var en = ['Ya','ya','Yu','yu','Ch','ch','Sh','sh','Sh','sh','Zh','zh','A','a','B','b','V','v','G','g','D','d','E','e','E','e','Z','z','I','i','J','j','K','k','L','l','M','m','N','n', 'O','o','P','p','R','r','S','s','T','t','U','u','F','f','H','h','C','c','Y','y','','','','','E', 'e'];
 
     for(var i=0, len = ru.length; i < len; i++){
         var reg = new RegExp(ru[i], "g");
@@ -15,6 +15,8 @@ $(function(){
 	$(document).on('click', '.more-lecture', function(){
 		var form = $(this).parents('.member-additional-form');
 		var count = $(this).parents('.form-block').find('.lecture-form').length;
+
+		$('.more-lecture').hide();
 
 		$.ajax({
 			url: '/lecture',
@@ -30,6 +32,7 @@ $(function(){
 				});
 				newform.find('.js-file-input').fileInput();
 				form.after(newform);
+				newform.find('input').first().focus();
 			}
 		});
 		
